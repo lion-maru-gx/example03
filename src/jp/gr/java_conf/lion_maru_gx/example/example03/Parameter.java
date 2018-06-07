14249d113c69b5d648625c2039ea387e6e400de5
lion-maru-gx <lion.maru.gx@gmail.com>
2018-06-07 14:39:28 +0900

    First Commit

---
+++ src/jp/gr/java_conf/lion_maru_gx/example/example03/Parameter.java
@@ -0,0 +1,6 @@
+package jp.gr.java_conf.lion_maru_gx.example.example03;
+
+public interface Parameter {
+	public Object get();
+	public void set(Object value);
+}
---
+++ src/jp/gr/java_conf/lion_maru_gx/example/example03/TopController.java
@@ -0,0 +1,20 @@
+package jp.gr.java_conf.lion_maru_gx.example.example03;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.fxml.Initializable;
+
+public class TopController implements Initializable {
+	private static final Logger logger = LoggerFactory.getLogger(TopController.class);
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		logger.info("%C:%M");
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
