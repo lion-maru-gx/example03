0b7e5d60e6af6213fe70c1da2f8aeb8f11ab58b3
lion-maru-gx <lion.maru.gx@gmail.com>
2018-06-21 10:47:11 +0900

    work

---
+++ src/jp/gr/java_conf/lion_maru_gx/example/example03/FB01VoicePatch.java
@@ -0,0 +1,14 @@
+package jp.gr.java_conf.lion_maru_gx.example.example03;
+
+import javax.sound.midi.Patch;
+
+public class FB01VoicePatch extends Patch {
+	private int param1=1;
+
+
+	public FB01VoicePatch(int arg0, int arg1) {
+		super(arg0, arg1);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
