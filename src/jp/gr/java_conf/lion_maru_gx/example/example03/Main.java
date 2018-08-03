5def8e1bcda13f937ab2ed04fa6d6d65043dc7ef
lion-maru-gx <lion.maru.gx@gmail.com>
2018-08-03 16:32:04 +0900

    エラー修正

--- src/jp/gr/java_conf/lion_maru_gx/example/example03/Main.java
+++ src/jp/gr/java_conf/lion_maru_gx/example/example03/Main.java
@@ -1,18 +1,18 @@
 package jp.gr.java_conf.lion_maru_gx.example.example03;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Top.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Pane root = (Pane)FXMLLoader.load(getClass().getResource("Top.fxml"));
+			Scene scene = new Scene(root,600,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -20,7 +20,7 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
